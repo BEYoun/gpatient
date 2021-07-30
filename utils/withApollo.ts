2aa365434279871f524b4601825715ee2c4499cf
younes beaila <beailayounes@gmail.com>
2021-07-30 20:24:44 +0100

    url server

--- utils/withApollo.ts
+++ utils/withApollo.ts
@@ -4,7 +4,7 @@ import { NextPageContext } from "next";
 
 const createClient = (ctx: NextPageContext) =>
   new ApolloClient({
-    uri: 'https://server.goodoc.co/graphql',
+    uri: 'http://server.goodoc.co/graphql',
     credentials: "include",
     headers: {
       cookie:
