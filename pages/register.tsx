7d650c36a86418ca5d62519689bc8845c07d8a18
younes beaila <beailayounes@gmail.com>
2021-08-24 17:25:31 +0100

    rectif

--- pages/register.tsx
+++ pages/register.tsx
@@ -182,15 +182,15 @@ const Register: React.FC = ({ }) => {
                     </label>
                                         <div className="mt-1">
                                             <input
-                                                type="confirmPassword"
+                                                type="password"
                                                 name="confirmPassword"
                                                 onChange={handleChange}
                                                 onBlur={handleBlur}
-                                                value={values.password}
+                                                value={values.confirmPassword}
                                                 required
                                                 className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
                                             />
-                                            {errors.password && touched.password && errors.password}
+                                            {errors.confirmPassword && touched.confirmPassword && errors.confirmPassword}
                                         </div>
                                     </div>
 
