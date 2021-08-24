5a0fd3acdd93eee823615f5e874c9ada4547da94
younes beaila <beailayounes@gmail.com>
2021-08-24 17:27:19 +0100

    rectif

--- pages/register.tsx
+++ pages/register.tsx
@@ -166,7 +166,7 @@ const Register: React.FC = ({ }) => {
                                                 name="password"
                                                 onChange={handleChange}
                                                 onBlur={handleBlur}
-                                                value={values.confirmPassword}
+                                                value={values.password}
                                                 required
                                                 className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
                                             />
