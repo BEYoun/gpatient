d80b5a18e5f36b6d27e267fa8a346535eb1e6a36
younes beaila <beailayounes@gmail.com>
2021-09-07 12:55:55 +0100

    zoom input 4

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,7 +29,6 @@ class MyDocument extends Document {
                             })(window,document,'script','dataLayer','GTM-MZ43ZNR');`
                         }}
                     />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
                 </Head>
                 <body>
                     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZ43ZNR"
